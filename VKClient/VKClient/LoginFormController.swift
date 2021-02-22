97525b37a518d6ef7c47417e61d8046e4a1c5879
mysimplenickname <lvmlkv@yandex.ru>
2021-02-22 20:35:39 +0300

    simple authorization added

--- VKClient/VKClient/LoginFormController.swift
+++ VKClient/VKClient/LoginFormController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class LoginFormController: UIViewController {
 
+    private let login = "admin",
+                password = "password"
+    
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func startTapped(_ sender: UIButton) {
-        print(#function)
+        if loginField.text == login && passwordField.text == password {
+            print("Logged in")
+        } else {
+            print("Wrong login or password")
+        }
     }
 }
