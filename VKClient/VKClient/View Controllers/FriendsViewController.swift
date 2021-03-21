1b49c21544a8b060b15cb85d239cd068e54ea2a1
mysimplenickname <lvmlkv@yandex.ru>
2021-03-21 22:03:01 +0300

    homework6

--- VKClient/VKClient/View Controllers/FriendsViewController.swift
+++ VKClient/VKClient/View Controllers/FriendsViewController.swift
@@ -7,17 +7,23 @@
 
 import UIKit
 
-class FriendsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class FriendsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
 
     let friends: [User] = User.loadUsers()
+    var friendsForUse: [User] = []
     
     // MARK: - Life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(FriendsCell.reusableId)
         tableView.register(UINib(nibName: "FriendsCell", bundle: nil), forCellReuseIdentifier: FriendsCell.reusableId)
+        
+        tableView.register(UINib(nibName: "FriendsHeaderView", bundle: nil), forHeaderFooterViewReuseIdentifier: FriendsHeaderView.reusableId)
+        
+        searchBar.delegate = self
+        
+        friendsForUse = friends
     }
 
     // MARK: - Segues
@@ -26,7 +32,7 @@ class FriendsViewController: UIViewController, UITableViewDataSource, UITableVie
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let controller = segue.destination as? PhotosViewController, let indexPath = tableView.indexPathForSelectedRow {
-            controller.images = friends[indexPath.row].images
+            controller.images = User.arrangeUsers(users: friendsForUse)[indexPath.section][indexPath.row].images
         }
     }
     
@@ -40,30 +46,65 @@ class FriendsViewController: UIViewController, UITableViewDataSource, UITableVie
     
     // MARK: - Table view data source
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return User.firstLetters(users: friendsForUse).count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return friends.count
+        return User.arrangeUsers(users: friendsForUse)[section].count
     }
     
     // MARK: - Table view delegate
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: FriendsCell.reusableId, for: indexPath) as! FriendsCell
-        cell.configureCell(friend: friends[indexPath.row])
+        cell.configureCell(friend: User.arrangeUsers(users: friendsForUse)[indexPath.section][indexPath.row])
         return cell
+    }
+    
+    // MARK: - Sections
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: FriendsHeaderView.reusableId) as? FriendsHeaderView
+        headerView?.textLabel?.text = String(User.firstLetters(users: friendsForUse)[section])
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return FriendsHeaderView.height
     }
     
     // MARK: - Actions
     
     @IBAction func choosenFriend(_ sender: FriendsSelectorControl) {
-        var index: UInt = 0
-        for friend in friends {
-            if sender.selectedValue?.surname == friend.surname {
-                break
+        var row: Int = 0,
+            section: Int = 0
+        
+        let arrangedUsers = User.arrangeUsers(users: friendsForUse)
+        
+        for i in 0..<arrangedUsers.count {
+            for j in 0..<arrangedUsers[i].count {
+                if arrangedUsers[i][j].surname == sender.selectedValue?.surname {
+                    row = j
+                    section = i
+                    break
+                }
             }
-            index += 1
         }
-        let indexPath = IndexPath(row: Int(index), section: 0)
+        
+        let indexPath = IndexPath(row: row, section: section)
         tableView.scrollToRow(at: indexPath, at: .top, animated: true)
     }
+    
+    // MARK: - Search bar
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            friendsForUse = searchText.isEmpty ? friends : friends.filter { (item: User) -> Bool in
+                return item.fullname.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+            }
+            tableView.reloadData()
+        }
     
 }
---
+++ VKClient/VKClient/View Controllers/NewsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  NewsViewController.swift
+//  VKClient
+//
+//  Created by Lev on 3/21/21.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.register(UINib(nibName: "NewsCell", bundle: nil), forCellReuseIdentifier: NewsCell.reusableId)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: NewsCell.reusableId, for: indexPath) as! NewsCell
+        cell.newsTextLabel?.text = "Hello!"
+        cell.newsImageView?.image = UIImage(named: "johnsnow1")
+        return cell
+    }
+
+}
