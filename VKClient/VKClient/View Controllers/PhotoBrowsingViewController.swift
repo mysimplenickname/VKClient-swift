a4743d517408f45594f7dca6b433c8ae3df0eb76
mysimplenickname <lvmlkv@yandex.ru>
2021-04-02 13:14:22 +0300

    it finally works

--- VKClient/VKClient/View Controllers/PhotoBrowsingViewController.swift
+++ VKClient/VKClient/View Controllers/PhotoBrowsingViewController.swift
@@ -17,52 +17,15 @@ class PhotoBrowsingViewController: UIViewController {
     
     private lazy var centerImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.image = UIImage(named: images[imagesIndex].name)
+        imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     
-    private lazy var leftImageView: UIImageView = {
+    private lazy var nextImageView: UIImageView = {
         let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
         return imageView
     }()
-    
-    private lazy var rightImageView: UIImageView = {
-        let imageView = UIImageView()
-        return imageView
-    }()
-    
-    private func layout() {
-        centerImageView.removeFromSuperview()
-        leftImageView.removeFromSuperview()
-        rightImageView.removeFromSuperview()
-        
-        centerImageView.translatesAutoresizingMaskIntoConstraints = false
-        centerContainerView.addSubview(centerImageView)
-        NSLayoutConstraint.activate([
-            centerImageView.topAnchor.constraint(equalTo: centerContainerView.topAnchor),
-            centerImageView.bottomAnchor.constraint(equalTo: centerContainerView.bottomAnchor),
-            centerImageView.leftAnchor.constraint(equalTo: centerContainerView.leftAnchor),
-            centerImageView.rightAnchor.constraint(equalTo: centerContainerView.rightAnchor)
-        ])
-        
-        leftImageView.translatesAutoresizingMaskIntoConstraints = false
-        centerContainerView.addSubview(leftImageView)
-        NSLayoutConstraint.activate([
-            leftImageView.topAnchor.constraint(equalTo: centerContainerView.topAnchor),
-            leftImageView.bottomAnchor.constraint(equalTo: centerContainerView.bottomAnchor),
-            leftImageView.leftAnchor.constraint(equalTo: centerContainerView.leftAnchor, constant: -centerContainerView.frame.width),
-            leftImageView.rightAnchor.constraint(equalTo: centerContainerView.leftAnchor)
-        ])
-        
-        rightImageView.translatesAutoresizingMaskIntoConstraints = false
-        centerContainerView.addSubview(rightImageView)
-        NSLayoutConstraint.activate([
-            rightImageView.topAnchor.constraint(equalTo: centerContainerView.topAnchor),
-            rightImageView.bottomAnchor.constraint(equalTo: centerContainerView.bottomAnchor),
-            rightImageView.leftAnchor.constraint(equalTo: centerContainerView.rightAnchor),
-            rightImageView.rightAnchor.constraint(equalTo: centerContainerView.rightAnchor, constant: centerContainerView.frame.width)
-        ])
-    }
     
     private lazy var panGestureRecognizer: UIPanGestureRecognizer = {
         let recognizer = UIPanGestureRecognizer()
@@ -73,7 +36,15 @@ class PhotoBrowsingViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        layout()
+        if imagesIndex < 0 && imagesIndex > images.count - 1 {
+            imagesIndex = 0
+        }
+        
+        centerImageView.image = UIImage(named: images[imagesIndex].name)
+        centerContainerView.addSubview(centerImageView)
+        centerImageView.frame = centerContainerView.bounds
+        
+        title = String("\(imagesIndex! + 1) of \(images.count)")
         
         centerContainerView.addGestureRecognizer(panGestureRecognizer)
     }
@@ -81,128 +52,157 @@ class PhotoBrowsingViewController: UIViewController {
     private enum Side {
         case left
         case right
-        case none
     }
     
-    private var currentState: Side = .none
+    private var currentState: Side!
     
     private var runningAnimators: [UIViewPropertyAnimator] = []
     
-    private func animateTransitionIfNeeded(to side: Side, duration: TimeInterval) {
+    private func animateTransitionIfNeeded(duration: TimeInterval) {
         
         guard runningAnimators.isEmpty else { return }
         
         let centerTransitionAnimator = UIViewPropertyAnimator(duration: duration, curve: .linear, animations: { [self] in
-            
-            switch side {
-            
-            case .left: // 1 -> 2
+            switch currentState {
+            case .left:
                 centerImageView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8).concatenating(CGAffineTransform(translationX: centerContainerView.frame.width, y: 0))
-                
-            case .right: // 2 <- 1
+            case .right:
                 centerImageView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8).concatenating(CGAffineTransform(translationX: -centerContainerView.frame.width, y: 0))
-            
             default:
                 break
-                
             }
-            
-            self.centerContainerView.layoutIfNeeded()
         })
         
-        let sideTransitionAnimator = UIViewPropertyAnimator(duration: duration, curve: .linear, animations: { [self] in
-            
-            switch side {
-            
-            case .left: // 1 -> 2
-                imagesIndex -= 1
-                leftImageView.image = UIImage(named: images[imagesIndex].name)
-                centerContainerView.bringSubviewToFront(leftImageView)
-                leftImageView.transform = CGAffineTransform(translationX: centerContainerView.frame.width, y: 0)
+        if canPerformTransition(state: currentState) {
+            centerTransitionAnimator.addCompletion { [self] (position) in
                 
-            case .right: // 2 <- 1
-                imagesIndex += 1
-                rightImageView.image = UIImage(named: images[imagesIndex].name)
-                centerContainerView.bringSubviewToFront(rightImageView)
-                rightImageView.transform = CGAffineTransform(translationX: -centerContainerView.frame.width, y: 0)
+                guard position == .end else { return }
             
-            default:
-                break
+                switch currentState {
+                case .left:
+                    imagesIndex -= 1
+                case .right:
+                    imagesIndex += 1
+                default:
+                    break
+                }
+                
+                centerImageView.transform = .identity
+                centerImageView.image = UIImage(named: images[imagesIndex].name)
+                
+                runningAnimators.remove(at: runningAnimators.firstIndex(of: centerTransitionAnimator)!)
                 
             }
-            
-            self.centerContainerView.layoutIfNeeded()
-        })
+        }
         
         centerTransitionAnimator.startAnimation()
-        sideTransitionAnimator.startAnimation()
-
         runningAnimators.append(centerTransitionAnimator)
-        runningAnimators.append(sideTransitionAnimator)
         
+        if canPerformTransition(state: currentState) {
+            
+            switch currentState {
+            case .left:
+                nextImageView.image = UIImage(named: images[imagesIndex - 1].name)
+                centerContainerView.addSubview(nextImageView)
+                nextImageView.frame = centerContainerView.bounds.offsetBy(dx: -centerContainerView.frame.width, dy: 0)
+            case .right:
+                nextImageView.image = UIImage(named: images[imagesIndex + 1].name)
+                centerContainerView.addSubview(nextImageView)
+                nextImageView.frame = centerContainerView.bounds.offsetBy(dx: centerContainerView.frame.width, dy: 0)
+            default:
+                break
+            }
+            
+            let sideTransitionAnimator = UIViewPropertyAnimator(duration: duration, curve: .linear, animations: { [self] in
+                switch currentState {
+                case .left:
+                    title = String("\(imagesIndex!) of \(images.count)")
+                    nextImageView.transform = CGAffineTransform(translationX: centerContainerView.frame.width, y: 0)
+                case .right:
+                    title = String("\(imagesIndex! + 2) of \(images.count)")
+                    nextImageView.transform = CGAffineTransform(translationX: -centerContainerView.frame.width, y: 0)
+                default:
+                    break
+                }
+            })
+            
+            sideTransitionAnimator.addCompletion { [self] (position) in
+                guard position == .end else { return }
+                
+                nextImageView.transform = .identity
+                nextImageView.removeFromSuperview()
+                
+                runningAnimators.remove(at: runningAnimators.firstIndex(of: sideTransitionAnimator)!)
+            }
+            
+            sideTransitionAnimator.startAnimation(afterDelay: 0.2)
+            runningAnimators.append(sideTransitionAnimator)
+            
+        }
     }
     
     private var shouldEnd: Bool = false
     
-    @objc private func imageViewPanned(recognizer: UIPanGestureRecognizer) {
+    @objc private func imageViewPanned(_ recognizer: UIPanGestureRecognizer) {
+        
+        var fraction = recognizer.translation(in: centerContainerView).x / centerContainerView.frame.width
+        currentState = fraction > 0 ? .left : .right
         
         switch recognizer.state {
-        
         case .began:
-            
-            animateTransitionIfNeeded(to: currentState, duration: 1)
-            
+            animateTransitionIfNeeded(duration: 1)
             runningAnimators.forEach {
                 $0.pauseAnimation()
             }
-        
+            
         case .changed:
-            let translation = recognizer.translation(in: centerContainerView)
-            let fraction = translation.x / centerContainerView.frame.width
-            
-            currentState = fraction > 0 ? (imagesIndex > 0 ? .left : .none) : (imagesIndex < images.count - 1 ? .right : .none)
-            
-            print(fraction, currentState, imagesIndex)
-            
+            fraction = recognizer.translation(in: centerContainerView).x / centerContainerView.frame.width
             runningAnimators.forEach {
-                $0.fractionComplete = fraction
+                $0.fractionComplete = abs(fraction)
             }
-            
-            shouldEnd = abs(fraction) > 0.3
+            shouldEnd = abs(fraction) > 0.5
             
         case .ended:
-            print(currentState, shouldEnd)
-            if currentState != .none && shouldEnd {
+            if canPerformTransition(state: currentState) && shouldEnd {
                 runningAnimators.forEach {
                     $0.continueAnimation(withTimingParameters: nil, durationFactor: 0)
                 }
-                //layout()
-                //centerContainerView.layoutIfNeeded()
-                break
+            } else {
+                runningAnimators.forEach {
+                    $0.stopAnimation(true)
+                    $0.finishAnimation(at: .current)
+                }
+                runningAnimators.removeAll()
+                
+                title = String("\(imagesIndex! + 1) of \(images.count)")
+                
+                UIView.animate(
+                    withDuration: 0.5,
+                    delay: 0,
+                    options: [.curveEaseOut],
+                    animations: { [self] in
+                        centerImageView.transform = .identity
+                        if canPerformTransition(state: currentState) {
+                            nextImageView.transform = .identity
+                        }
+                    },
+                    completion: { [self] _ in
+                        if canPerformTransition(state: currentState) {
+                            nextImageView.removeFromSuperview()
+                        }
+                    }
+                )
             }
-            
-            runningAnimators.forEach {
-                $0.stopAnimation(true)
-                $0.finishAnimation(at: .current)
-            }
-            runningAnimators.removeAll()
-            
-            UIView.animate(
-                withDuration: 1,
-                delay: 0,
-                options: [.curveEaseOut],
-                animations: {
-                    self.centerImageView.transform = .identity
-                    self.leftImageView.transform = .identity
-                    self.rightImageView.transform = .identity
-                },
-                completion: nil
-            )
             
         default:
             break
-            
         }
+    }
+    
+    private func canPerformTransition(state: Side) -> Bool {
+        return state == .left
+            ? imagesIndex > 0
+            : imagesIndex < images.count - 1
     }
     
 }
